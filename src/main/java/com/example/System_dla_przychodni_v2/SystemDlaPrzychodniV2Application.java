4f253655a919843efa2404c906a4718ebbc40f19
Mateusz Wróblewski <73337576+Matiu-dev@users.noreply.github.com>
2024-06-05 00:17:35 +0200

    Add files via upload

---
+++ src/main/java/com/example/System_dla_przychodni_v2/SystemDlaPrzychodniV2Application.java
@@ -0,0 +1,13 @@
+package com.example.System_dla_przychodni_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDlaPrzychodniV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDlaPrzychodniV2Application.class, args);
+	}
+
+}
